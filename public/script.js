c2e720e610f33302d111a96de639681e9231532c
CodeMonk21 <jagritmadaan2001@gmail.com>
2023-12-20 14:39:16 +0530

    update question complete

--- public/script.js
+++ public/script.js
@@ -23,28 +23,86 @@ const questionArr = [
         question: "Which of the following methods can be used to display data in some form using Javascript?",
         options:["document.write()","console.log()","window.alert()","All of the above"],
         correctAns:"All of the above"
+    },
+    {
+        question: "How can a datatype be declared to be a constant type?",
+        options:["const","var","let","constant"],
+        correctAns:"const"
+    },
+    {
+        question: "What keyword is used to check whether a given property is valid or not?",
+        options:["in","is in","exist","lies"],
+        correctAns:"in"
+    },
+    {
+        question: "When an operator’s value is NULL, the typeof returned by the unary operator is:",
+        options:["Boolean","Undefined","Object","Integer"],
+        correctAns:"Object"
+    },
+    {
+        question: "What does the Javascript “debugger” statement do?",
+        options:["It will debug all the errors in the program at runtime.","It acts as a breakpoint in a program.","It will debug error in the current statement if any.","All of the above."],
+        correctAns:"It acts as a breakpoint in a program."
+    },
+    {
+        question:"What does the ‘toLocateString()’ method do in JS?",
+        options:["Returns a localised object representation.","Returns a parsed string.","Returns a localized string representation of an object.","None of the above."],
+        correctAns:"Returns a localized string representation of an object."
+    },
+    {
+        question:"Which function is used to serialize an object into a JSON string in Javascript?",
+        options:["stringify()","parse()","convert()","None of the above."],
+        correctAns:"stringify()"
     }
 ]
-
 
 const optionElements = document.querySelectorAll(".option")
 const nextBtn = document.querySelector("#next-btn")
 let  questionNo = 1
+let attemptQuestion  = 0
 let  score = 0
 let currentQuestion = questionArr[questionNo-1]
 
 
+//Check Next button is clicked and update question & options
+nextBtn.addEventListener("click",()=>{
+    if(questionNo>=10){
+        alert(`You Scored:- ${score}`)
+    }
+    else if(attemptQuestion==questionNo){
+        document.querySelector("#question-no").innerHTML = ++questionNo
+        currentQuestion = questionArr[questionNo-1]
+        updateQuestion()
+    }else{
+        alert(`Please first attempt question no. ${questionNo}`)
+    }
+})
+
 //Check if the any given options is clicked:And update Score
 optionElements.forEach((option)=>option.addEventListener("click",()=>{
     if(checkAnswer(option)==true){
+        //update score: for correct answer
         document.querySelector("#score-no").innerHTML = ++score
     }
+    //disabled all options element
+    attemptQuestion++
     optionElements.forEach((element)=> element.disabled = true)
 }))
 
+//fn:update question and its options
+function updateQuestion(){
+    document.querySelector("#question").innerText = currentQuestion.question
+    optionElements.forEach((option,index)=>{
+        option.textContent = currentQuestion.options[index]
+        //Remove styling of previous question 
+        option.disabled = false
+        option.classList.remove("bg-green-500","text-white","border-0","hover:border-green-500")
+        option.classList.remove("bg-red-500","text-white","border-0","hover:border-red-500")
+    })
+}
 //fn:Check if clicked option is correct or not: And highlight it
 function checkAnswer(optionClicked){
-    const optionValue = optionClicked.innerText.toUpperCase()
+    const optionValue = optionClicked.innerText
     if(optionValue==currentQuestion.correctAns){
         optionClicked.classList.add("bg-green-500","text-white","border-0","hover:border-green-500")
         return true
